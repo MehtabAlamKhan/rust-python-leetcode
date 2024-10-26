b1494e86d7c5ab6811e0224745288b27b5f00bfc
MehtabAlamKhan <kmehtabalam19@gmail.com>
2024-10-26 13:26:45 +0530

    62 Rust

---
+++ 62. Unique Paths/simple.rs
@@ -0,0 +1,21 @@
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn unique_paths(m: i32, n: i32) -> i32 {
+        return Self::gridTraveler(&m, &n, 0, 0, &mut HashMap::new())
+    }
+    pub fn gridTraveler(m: &i32, n: &i32, i: i32, j: i32, memo: &mut HashMap<(i32, i32), i32>)->i32{
+        if let Some(&cached) = memo.get(&(i,j)){
+            return cached
+        }
+        if i == *m - 1 && j == *n - 1{
+            return 1;
+        }
+        if i >= *m || j >= *n {
+            return 0;
+        }
+        let paths = Self::gridTraveler(m, n, i + 1, j, memo) + Self::gridTraveler(m, n, i, j + 1, memo);
+        memo.insert((i,j), paths);
+        return paths
+    }
+}
