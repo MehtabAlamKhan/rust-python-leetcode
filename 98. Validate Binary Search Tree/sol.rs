a49a2a1bbe067b1ad345c71105e03c8da6fc8ddb
MehtabAlamKhan <kmehtabalam19@gmail.com>
2024-11-20 00:14:52 +0530

    98

--- 98. Validate Binary Search Tree/sol.rs
+++ 98. Validate Binary Search Tree/sol.rs
@@ -22,19 +22,18 @@ impl Solution {
     pub fn is_valid_bst(root: Option<Rc<RefCell<TreeNode>>>) -> bool {
         let mut root = root;
         let mut q: Vec<Option<Rc<RefCell<TreeNode>>>> = vec![];
-        let mut prev: Option<Rc<RefCell<TreeNode>>> = None;
+        let mut prev: Option<i32> = None;
         while !q.is_empty() || root.is_some() {
             while let Some(node_rc) = root {
-                prev = Some(node_rc);
                 q.push(Some(node_rc.clone()));
                 root = node_rc.borrow().left.clone();
             }
             if let Some(Some(node_rc)) = q.pop() {
                 let node_ref = node_rc.borrow();
-                if node_ref.val > prev.unwrap().borrow().val {
+                if prev.is_some() && node_ref.val <= prev.unwrap() {
                     return false;
                 }
-                prev = Some(node_rc);
+                prev = Some(node_rc.borrow().val);
                 root = node_ref.right.clone();
             }
         }
