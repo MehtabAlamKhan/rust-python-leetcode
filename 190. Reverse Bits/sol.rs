277380763762f9dc0e8485e37ae26faca9307912
MehtabAlamKhan <kmehtabalam19@gmail.com>
2024-11-25 06:11:27 +0530

    190

---
+++ 190. Reverse Bits/sol.rs
@@ -0,0 +1,11 @@
+impl Solution {
+    pub fn reverse_bits(mut x: u32) -> u32 {
+        let mut x = x;
+        let mut res = 0;
+        for _ in 0..32 {
+            res = (res << 1) | (x & 1);
+            x >>= 1;
+        }
+        res
+    }
+}
